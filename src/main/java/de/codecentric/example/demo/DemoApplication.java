9bb49cccb112eba22511c6188ece9f4c41b5e30a
Henning Waack <henning.waack@codecentric.de>
2018-09-28 09:03:31 +0200

    added springfox swagger ui to project

--- src/main/java/de/codecentric/example/demo/DemoApplication.java
+++ src/main/java/de/codecentric/example/demo/DemoApplication.java
@@ -2,11 +2,40 @@ package de.codecentric.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+import java.time.LocalDate;
+
+@SpringBootApplication(scanBasePackages = "de.codecentric.example.demo")
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+        MyRestController.class
+})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage( "de.codecentric.example.demo" ))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false)
+                //.securitySchemes(newArrayList(new BasicAuth("test")))
+                .enableUrlTemplating(true);
+    }
 }
--- src/main/java/de/codecentric/example/demo/SecurityConfig.java
+++ src/main/java/de/codecentric/example/demo/SecurityConfig.java
@@ -4,24 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST_SPRINGFOX = {
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http.csrf().disable();
         http.httpBasic();
+        // auth for REST api
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/user").hasRole("admin")
-                .antMatchers(HttpMethod.GET, "/user/*").hasAnyRole("admin", "user");
+                .antMatchers(HttpMethod.GET, "/user/*").hasAnyRole("admin", "user")
+                .antMatchers(AUTH_WHITELIST_SPRINGFOX).hasAnyRole("user", "admin");
+        // auth for swagger ui
+        http.authorizeRequests()
+                .antMatchers(AUTH_WHITELIST_SPRINGFOX).hasAnyRole("user", "admin");
         http.authorizeRequests().anyRequest().fullyAuthenticated();
-        http.csrf().disable();
     }
 
     @Override
@@ -29,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user").password("userpw").roles("user")
                 .and()
-                .withUser("admin").password("adminpw").roles("admin");
+                .withUser("admin").password("adminpw").roles("user", "admin");
     }
 
     @SuppressWarnings("deprecation")
